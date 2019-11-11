052f6439c3e89fd066257bf98b5f1d9928d5361d
Craig P. Motlin <cmotlin@gmail.com>
2019-11-11 08:48:33 -0500

    Change with(e1, e2) and with(e1, e2, e3) to delegate to with(e) to make them inlineable.

--- eclipse-collections/src/main/java/org/eclipse/collections/impl/bag/sorted/mutable/TreeBag.java
+++ eclipse-collections/src/main/java/org/eclipse/collections/impl/bag/sorted/mutable/TreeBag.java
@@ -713,9 +713,7 @@ public class TreeBag<T>
 
     public MutableSortedBag<T> with(T element1, T element2)
     {
-        this.add(element1);
-        this.add(element2);
-        return this;
+        return this.with(element1).with(element2);
     }
 
     @Override
@@ -729,10 +727,7 @@ public class TreeBag<T>
 
     public MutableSortedBag<T> with(T element1, T element2, T element3)
     {
-        this.add(element1);
-        this.add(element2);
-        this.add(element3);
-        return this;
+        return this.with(element1).with(element2).with(element3);
     }
 
     @Override
